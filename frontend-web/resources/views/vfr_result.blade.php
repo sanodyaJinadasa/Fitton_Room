ee19c9f9a5d964ce0fae234499aaf70c42c98ee9
P.M.S.V. Jinadasa <sanodyaj@gmail.com>
2026-08-26 22:27:43 +0530

    Update vfr_result.blade.php

--- frontend-web/resources/views/vfr_result.blade.php
+++ frontend-web/resources/views/vfr_result.blade.php
@@ -1,3 +1,3 @@
-<h2>Your Virtual Fitting Result</h2>
+<!-- <h2>Your Virtual Fitting Result</h2>
 <img src="data:image/png;base64,{{ $image }}" alt="Result"><br>
-<a href="{{ url('/') }}">Try Another</a>
+<a href="{{ url('/') }}">Try Another</a> -->
